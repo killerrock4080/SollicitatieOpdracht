d8eac2b28b1f4467cf4cf1f7bee14400e31ffacd
Ryan <Ryan.hertogs@gmail.com>
2022-03-29 22:05:41 +0200

    Angular front end sollicitatieopdracht

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,5 @@
+import { HttpClient } from '@angular/common/http';
+import { identifierName } from '@angular/compiler';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public responsedata?: Task[];
+  let clicked = false;
+
+  constructor(http: HttpClient) {
+    http.get<Task[]>('https://jsonplaceholder.typicode.com/posts').subscribe(result => {
+      this.responsedata = result;
+      console.log(result);
+    }, error => console.error(error));
+  }
+
   title = 'SollicitatieOpdracht';
+}
+
+
+loadDetails(http: HttpClient, id){
+  http.get<Task[]>('https://jsonplaceholder.typicode.com/posts/{{id}}/comments').subscribe(result => {
+    this.responsedata = result;
+    console.log(result);
+  }, error => console.error(error));
+}
+
+interface Task {
+  userid: number;
+  id: number;
+  title: string;
+  body: string;
 }
---
+++ src/app/card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+
+/**
+ * @title Basic cards
+ */
+@Component({
+  selector: 'card-overview-example',
+  templateUrl: 'card.component.html',
+})
+export class CardOverview { }
